d024697b59714ec30f408a1ce67b3e5b1ea30874
kiselevgleb <kiselevgleb@gmail.com>
2020-06-16 13:44:25 +0300

    added setTimeout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-﻿
+import './css/task.css';
+import './js/app';
+// import './index.html';
+// require('html-loader!./index.html');
--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,34 @@
-let but = document.querySelectorAll(".hole");
-
+const divParent = document.createElement('div');
 function getRandomInt() {
-    return Math.floor(Math.random() * 16);
+  return Math.floor(Math.random() * 16);
 }
 let i;
 let rand;
+let classParent = "hole-game";
+let c = "hole";
+divParent.classList.add(classParent);
+document.body.appendChild(divParent);
+for (let index = 0; index < 16; index++) {
+  const divEl = document.createElement('div');
+  divEl.classList.add(c);
+  divEl.setAttribute("id", index);
+  divParent.appendChild(divEl);
+}
+  console.log(document.querySelectorAll('.hole'));
+  const but = document.querySelectorAll('.hole');
 setInterval(() => {
-    for (let index = 0; index < but.length; index++) {
-        const element = but[index];
-        element.classList.remove("hole_has-mole");
-        i = index;
-    }
+  for (let index = 0; index < but.length; index++) {
+    const element = but[index];
+    if(element.classList.contains('hole_has-mole')){
+    element.classList.remove('hole_has-mole');
+    i = index;}
+  }
 }, 1000);
 
 setInterval(() => {
-        rand = getRandomInt();
-        console.log(rand);
-        while (i == rand) {
-            rand = getRandomInt();
-            console.log("ryyyyyyy"+rand);
-        }
-        but[rand].className = 'hole hole_has-mole'}, 1000);
+  rand = getRandomInt();
+  while (i === rand) {
+    rand = getRandomInt();
+  }
+  but[rand].className = 'hole hole_has-mole';
+}, 1000);
